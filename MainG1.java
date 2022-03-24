630adc32ec52c951952068cc59c8f7ccc939528d
SofiLam13 <87708580+SofiLam13@users.noreply.github.com>
2022-03-24 07:43:09 -0600

    Creación del Main

---
+++ MainG1.java
@@ -0,0 +1,14 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Algoritmos y estructura de datos
+ * Hoja de trabajo 7
+ * Grupo 1
+ * Daniel Gómez 21429
+ * Sofi Lam 21548
+ */
+public class MainG1 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
